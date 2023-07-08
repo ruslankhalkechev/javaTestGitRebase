6a7cd633de54f9bed4e6845281a9ec8d348110d7
Ruslan Khalkechev <syrus@list.ru>
2023-07-08 12:26:22 +0300

    Второй commit task1

--- src/main/java/task1/Task1.java
+++ src/main/java/task1/Task1.java
@@ -2,6 +2,6 @@ package task1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Первый commit task1");
+        System.out.println("Второй commit task1");
     }
 }
