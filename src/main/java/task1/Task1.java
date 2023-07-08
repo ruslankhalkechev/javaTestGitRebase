fd5d3702bc879b4c723201b9100981a28419eef8
Ruslan Khalkechev <syrus@list.ru>
2023-07-08 13:57:22 +0300

    Task1 второй коммит

--- src/main/java/task1/Task1.java
+++ src/main/java/task1/Task1.java
@@ -2,6 +2,6 @@ package task1;
 
 public class Task1 {
     public static void main(String[] args) {
-        System.out.println("Task1 первый коммит");
+        System.out.println("Task1 второй коммит");
     }
 }
